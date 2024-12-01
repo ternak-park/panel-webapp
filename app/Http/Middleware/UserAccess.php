ad99f18a76ed866dd6a615878124b1bbe84f91d9
xXehub <artixsanni@gmail.com>
2024-12-01 18:33:11 +0700

    Update: Return view error

--- app/Http/Middleware/UserAccess.php
+++ app/Http/Middleware/UserAccess.php
@@ -15,7 +15,7 @@ class UserAccess
      */
     public function handle(Request $request, Closure $next, $userType): Response
     {
-        if(auth()->user()->type == $userType){
+        if (auth()->user()->type == $userType) {
             return $next($request);
         }
         // return response()->view('errors.404', [], 404);
