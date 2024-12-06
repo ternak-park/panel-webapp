c2155dbc247a5d28d644af63e650144d349feb10
xXehub <artixsanni@gmail.com>
2024-12-06 10:11:34 +0700

    Fix: Responsive navbar login button

--- resources/views/components/client-navbar.blade.php
+++ resources/views/components/client-navbar.blade.php
@@ -18,12 +18,11 @@
                            <a href="{{ route('register') }}" class="btn btn-outline-primary" rel="noreferrer">
                                Daftar
                            </a>
-                           <a href="{{ route('login') }}" class="btn btn-primary" rel="noreferrer">
-                               <!-- Download SVG icon from http://tabler-icons.io/i/heart -->
-                               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                           <a href="{{ route('login') }}" class="btn btn-primary d-flex align-items-center"
+                               rel="noreferrer">
+                               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                   stroke-linejoin="round"
-                                   class="icon icon-tabler icons-tabler-outline icon-tabler-login-2">
+                                   stroke-linejoin="round" class="icon me-1">
                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                    <path
                                        d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
@@ -34,8 +33,21 @@
                            </a>
                        </div>
                    </div>
+                   <div class="nav-item d-md-none">
+                       <a href="{{ route('login') }}" class="btn btn-primary d-sm-none btn-icon">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                               fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                               stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-login-2">
+                               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                               <path d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+                               <path d="M3 12h13l-3 -3" />
+                               <path d="M13 15l3 -3" />
+                           </svg>
+                       </a>
+                   </div>
                </div>
            @endguest
+
 
            @auth
                <div class="navbar-nav flex-row order-md-last">
